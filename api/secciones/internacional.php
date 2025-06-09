798412e5e707832cffa16a1860f6253c2a0130d5
FredyArana <163354854+FredyArana@users.noreply.github.com>
2025-06-09 12:13:16 -0500

    internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "Ecuador",
-"autor" => "Berpi",
-"resumen" => "Berpi quiere viajar a Ecuador",
+    "titulo" => "Perú alcanza récord histórico en exportaciones agrícolas",
+    "autor" => "María López",
+    "resumen" => "Perú reporta un aumento significativo en sus exportaciones agrícolas durante el último trimestre, consolidándose como uno de los principales proveedores en la región.",
 ];
 ?>

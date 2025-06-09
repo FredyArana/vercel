cc124bfa2805fc88553b673ba2d45f9f2a35f619
FredyArana <163354854+FredyArana@users.noreply.github.com>
2025-06-09 12:14:23 -0500

    tecnologia.php

--- api/secciones/tecnologia.php
+++ api/secciones/tecnologia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $tecnologia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+  "titulo" => "Lanzan nuevo smartphone con tecnología 5G en Perú",
+  "autor" => "Ana Martínez",
+  "resumen" => "Una reconocida marca presenta su nuevo smartphone compatible con redes 5G, prometiendo mayor velocidad y mejor conectividad para los usuarios peruanos.",
 ];
 ?>

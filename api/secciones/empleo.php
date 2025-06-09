d5e3d12fb88cda71ca35f9abdd360477df1f33e1
FredyArana <163354854+FredyArana@users.noreply.github.com>
2025-06-09 12:15:14 -0500

    empleo.php

--- api/secciones/empleo.php
+++ api/secciones/empleo.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $empleo = [
-"titulo" => "ANDROID DEVELOPER",
-"autor" => "Arana",
-"resumen" => "Arana maneja todo Kotlin",
+  "titulo" => "Android Developer",
+  "autor" => "Arana",
+  "resumen" => "Arana es un desarrollador Android experto en Kotlin, con más de 5 años de experiencia en desarrollo de aplicaciones móviles para startups y grandes empresas. Actualmente busca nuevas oportunidades para liderar proyectos innovadores y mejorar la experiencia del usuario.",
 ];
 ?>

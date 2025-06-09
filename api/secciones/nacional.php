6ed783d2788d81a5e54789714885f14d37f4a85f
FredyArana <163354854+FredyArana@users.noreply.github.com>
2025-06-09 12:13:51 -0500

    nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "PERÚ",
-"autor" => "FREDY",
-"resumen" => "Fredy Estudia en la UNS",
+  "titulo" => "Perú impulsa plan de reforestación nacional",
+  "autor" => "Fredy",
+  "resumen" => "El gobierno peruano lanza un ambicioso programa para reforestar miles de hectáreas en todo el país y combatir el cambio climático.",
 ];
 ?>

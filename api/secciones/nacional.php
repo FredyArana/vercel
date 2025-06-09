c353376ccb3ac6e7e1ba0e43b3fc7f6c04a8bfd0
FredyArana <163354854+FredyArana@users.noreply.github.com>
2025-06-09 12:11:08 -0500

    nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "PERÚ",
+"autor" => "FREDY",
+"resumen" => "Fredy Estudia en la UNS",
 ];
 ?>

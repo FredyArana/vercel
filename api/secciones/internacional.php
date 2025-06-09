b6bfa6d14a1f786c5bc1067318b611cfe527f602
FredyArana <163354854+FredyArana@users.noreply.github.com>
2025-06-09 12:11:32 -0500

    internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Ecuador",
+"autor" => "Berpi",
+"resumen" => "Berpi quiere viajar a Ecuador",
 ];
 ?>
